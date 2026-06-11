870f77f5d37d5a002e52e29c7c2e4ca45f74721e
Sarthak Verma <sarthakverma110@gmail.com>
2026-06-11 15:33:32 +0530

    Refactor IntersectionObserver for animations and counter

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const options = {
     threshold: 0.25
 };
 
-const observer = new IntersectionObserver((entries) => {
+const animationObserver = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
@@ -12,7 +12,43 @@ const observer = new IntersectionObserver((entries) => {
     });
 }, options);
 
+//Total Number of people donated counter
+
+let hasStarted=false
+const counter=document.getElementById('donation-received')
+const counterObserver=new IntersectionObserver((entries)=>
+{
+    
+    entries.forEach((entry)=>
+    {
+        
+        if(entry.isIntersecting && !hasStarted)
+        {
+            hasStarted=true
+            let count=0
+            const target=300
+                const timer=setInterval(()=>
+                {
+                    if(count>=target)
+                    {
+                        clearInterval(timer)
+                        counterObserver.disconnect()
+        
+                    }
+                    else
+                    {
+                        count++
+                    }
+
+                    counter.textContent=`${count}+`
+                },10)
+        }
+    })
+},options)
+
+
 document.addEventListener("DOMContentLoaded", () => {
-    observer.observe(document.querySelector('#moto'));
-    observer.observe(document.querySelector('.summarize'));
+    animationObserver.observe(document.querySelector('#moto'));
+    animationObserver.observe(document.querySelector('.summarize'));
+    counterObserver.observe(document.querySelector('.leftside-leftpart-container'))
 });
