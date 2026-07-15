f5287578636ce13f02be74bbb98f04d7562c02cf
Sarthak Verma <sarthakverma110@gmail.com>
2026-07-16 00:29:23 +0530

    Resolved merge conflicts

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const options = {
     threshold: 0.25
 };
@@ -53,59 +52,3 @@ document.addEventListener("DOMContentLoaded", () => {
     animationObserver.observe(document.querySelector('.summarize'));
     counterObserver.observe(document.querySelector('.leftside-leftpart-container'))
 });
-=======
-const options = {
-    threshold: 0.25
-};
-
-const animationObserver = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
-}, options);
-
-//Total Number of people donated counter
-
-let hasStarted=false
-const counter=document.getElementById('donation-received')
-const counterObserver=new IntersectionObserver((entries)=>
-{
-    
-    entries.forEach((entry)=>
-    {
-        
-        if(entry.isIntersecting && !hasStarted)
-        {
-            hasStarted=true
-            let count=0
-            const target=300
-                const timer=setInterval(()=>
-                {
-                    if(count>=target)
-                    {
-                        clearInterval(timer)
-                        counterObserver.disconnect()
-        
-                    }
-                    else
-                    {
-                        count++
-                    }
-
-                    counter.textContent=`${count}+`
-                },10)
-        }
-    })
-},options)
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    animationObserver.observe(document.querySelector('#moto'));
-    animationObserver.observe(document.querySelector('.summarize'));
-    counterObserver.observe(document.querySelector('.leftside-leftpart-container'))
-});
->>>>>>> a969abf325108f919bfe6f7275fa6b76e507d7cf
